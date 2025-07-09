292770f39ab92b646754916950f4852d09626726
Gizem Bölükbaş <gizembolukbas200@gmail.com>
2025-07-10 02:08:58 +0300

    hatalar düzeltildi ve iyileştirmeler yapıldı

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home"),
     path("blogs/", views.blogs, name="blogs"),
-    path("blogs/<slug:slug>/", views.blog_detail, name="blog_detail"),
+    path("blogs/<int:id>/", views.blog_detail, name="blog_detail"),
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,55 +1,14 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from blog.models import Blog
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
 
-
-data={
-    "blogs": [
-        {
-            "id":1,
-            "title":"komple web geliştime",
-            "image":"4.webp",
-             "is_active":True,
-             "is_home":False,
-             "description":"çok iyi bir kurs"
-        },
-
-           {
-            "id":2,
-            "title":"python kursu",
-            "image":"2.webp",
-             "is_active":True,
-              "is_home":True,
-             "description":"çok iyi bir kurs"
-        },
-
-           {
-            "id":3,
-            "title":"Django kursu",
-            "image":"3.webp",
-             "is_active":False,
-              "is_home":True,
-             "description":"çok iyi bir kurs"
-        },
-    ]
-}
-
-# views.py'da context ekleyin
 def index(request):
-    context = {
-        "blogs":Blog.objects.all
-    }
+    context = {"blogs": Blog.objects.filter(is_active=True)}
     return render(request, "blog/index.html", context)
 
 def blogs(request):
-    
-    return render(request,"blog/blogs.html")
-def blog_detail(request, id):
-    blogs = data["blogs"]
-    selectedBlog = None
+    context = {"blogs": Blog.objects.filter(is_active=True)}
+    return render(request, "blog/blogs.html", context)
 
-    for blog in blogs:
-        if blog["id"] == id:
-            selectedBlog = blog
-    
-    return render(request, "blog/blog-details.html", {"blog": selectedBlog})
+def blog_detail(request, id):  # ✅ Artık slug alıyor
+    blog = get_object_or_404(Blog, id=id)  # ✅ Veritabanından slug'a göre bul
+    return render(request, "blog/blog-details.html", {"blog": blog})
